b51aed2180d35421068e0712a5a718386433d737
shasi <shasivavilapalli@gmail.com>
2022-11-19 10:31:41 +0000

    my first java project

---
+++ home.java
@@ -0,0 +1,32 @@
+class devops
+
+public static voi main (string args[]){
+
+
+   system.oout.println("hello world");
+
+
+ system.oout.println("hello yaswaanth");
+
+ system.oout.println("hello manikanta");
+
+ system.oout.println("hello ramesh");
+
+ system.oout.println("hello rajesh");
+
+ system.oout.println("hello mahesh");
+
+ system.oout.println("hello world");
+
+ system.oout.println("hello world");
+
+ system.oout.println("hello world");
+
+ system.oout.println("hello world");
+
+
+
+
+}
+ system.oout.println("hello world");
+
